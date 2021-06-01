26ddb88fdc958ea9c2306be21900c69baebf914e
mr-eyes <mabuelanin@gmail.com>
2021-06-01 14:19:26 +0200

    download done

--- src/geneMatcher.py
+++ src/geneMatcher.py
@@ -2,11 +2,13 @@ import sys
 import click
 
 from src.click_context import cli
-from src.module_indexing import kmers as index_kmers       # pylint: disable=relative-beyond-top-level
+from src.module_indexing import kmers as index_kmers
+from src.module_download import download
 
 
 
 cli.add_command(index_kmers, name="index")
+cli.add_command(download, name="download")
 
 
 if __name__ == '__main__':
