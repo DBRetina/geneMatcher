c7537e425d5b6944de3cea16d436be78cb1560af
mr-eyes <mabuelanin@gmail.com>
2021-06-01 14:42:33 +0200

    download & extract

--- src/module_download.py
+++ src/module_download.py
@@ -14,10 +14,36 @@ def open_url(ctx, param, value):
 
 @cli.command(name="download", help_priority=2)
 @click.option('-u', '--url', "url", required=True, help="URL")
-@click.option('-o', '--output', "output", required=False, default=None, help="download output file")
+@click.option('-o', '--output', "output", required=False, default=None, help="download output file (omit gz to uncompress)")
 @click.pass_context
 def download(ctx, url, output):
     '''Downloading module'''
     LOGGER = ctx.obj
-    LOGGER.INFO(f"Downloading {url} as {output}")
-    subprocess.call(['wget', '-nc', f'{url}', '-O', f'{output}'])
+
+    compressed = False
+
+    if not len(output):
+        output = os.path.basename(url)
+        compressed = True
+    else:
+        if "gz" in output:
+            # compressed
+            compressed = True
+        else:
+            # uncompressed
+            compressed = False
+
+    if compressed:
+        LOGGER.INFO(f"Downloading {url} as {output}")
+        subprocess.call(['wget', '-nc', f'{url}', '-O', f'{output}'])
+
+    if not compressed:
+        LOGGER.INFO(f"Downloading & Extracting {url} as {output}")
+        subprocess.call(['wget', '-nc', f'{url}', '-O', f'{output}.gz'])
+        f = open(output, "w")
+        LOGGER.INFO(f"Extracting {output}")
+        subprocess.call(["gunzip", "-c", f'{output}.gz'], stdout=f)
+        f.close()
+        LOGGER.INFO(f"Deleting the copmpressed file")
+        subprocess.call(["rm", "-rf", f'{output}.gz'])
+    
